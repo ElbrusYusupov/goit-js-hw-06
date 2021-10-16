b8054d8d9850e6d1074920e6f723ca48a812ceb1
ElbrusYusupov <yusupoffup@gmail.com>
2021-10-16 20:56:15 +0300

    jghj

--- js/task-01.js
+++ js/task-01.js
@@ -1,2 +1,19 @@
 const categoriesEl = document.querySelector('#categories')
-console.log(categoriesEl.parentNode);
+console.log('Number of categories: ', categoriesEl.children.length);
+
+const itemEl = document.querySelector('.item');
+
+const textTitle = itemEl.querySelector('h2').textContent
+
+
+const amountElements = itemEl.querySelectorAll('li').length
+
+
+const menuItemsByTag = document.querySelectorAll(".item")
+
+const show  = () => {
+console.log(`Category: ${textTitle}`);
+console.log(`Elements: ${amountElements}`);
+};
+
+show()
